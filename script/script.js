0c0a9ccc6e7d4d3aa337c9c2ceac491196353411
snakeneax <kuzmin.snakeneax@yandex.ru>
2022-05-13 22:16:48 +0500

    final

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
-let editButton = document.querySelector ('.profile__edit');
-let popupEdit = document.querySelector ('#popupEdit');
-let closeBtnEdit = document.querySelector ('.popup__exit_edit');
-let formElementEdit = document.querySelector ('.popup__form_type_edit');
-let namePopup = document.querySelector('.popup__input_type_name');
-let jobPopup = document.querySelector('.popup__input_type_profession');
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let popupForm = document.querySelector('.popup__form')
-let AddButton = document.querySelector ('.profile__button');
-let popupAddCard = document.querySelector ('#popupAddCard');
-let closeBtnAdd = document.querySelector ('.popup__exit_add');
-let elements = document.querySelector ('.elements');
-let inputPlace = document.querySelector ('.popup__input_type_place');
-let inputImage = document.querySelector ('.popup__input_type_image');
+const editButton = document.querySelector ('.profile__edit');
+const popupEdit = document.querySelector ('#popupEdit');
+const closeBtnEdit = document.querySelector ('.popup__exit_edit');
+const formElementEdit = document.querySelector ('.popup__form_type_edit');
+const namePopup = document.querySelector('.popup__input_type_name');
+const jobPopup = document.querySelector('.popup__input_type_profession');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const popupForm = document.querySelector('.popup__form');
+const AddButton = document.querySelector ('.profile__button');
+const popupAddCard = document.querySelector ('#popupAddCard');
+const popupZoomCard = document.querySelector ('#popup__ShowPhoto');
+const popupZoomPhoto = popupZoomCard.querySelector ('.popup__image');
+const popupZoomTitle = popupZoomCard.querySelector ('.popup__image-caption')
+const closeBtnAdd = document.querySelector ('.popup__exit_add');
+const elements = document.querySelector ('.elements');
+const inputPlace = document.querySelector ('.popup__input_type_place');
+const inputImage = document.querySelector ('.popup__input_type_image');
+const cardsTemplate = document.querySelector("#cardTemplate").content;
+const addCard = document.querySelector('.popup__save');
+const ClosePopupImg = popupZoomCard.querySelector('.popup__exit_add')
 const initialCards = [
   {
     name: 'Архыз',
@@ -48,6 +54,14 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
 
+function openAddModal() {
+  openPopup(popupAddCard);
+};
+
+function render() {
+  cardsInfo.forEach(renderCards);
+};
+
 function formSubmitHandlerEdit (evt) {
   evt.preventDefault();
   profileName.textContent = namePopup.value;
@@ -61,19 +75,30 @@ function openEditModal() {
   jobPopup.value = profileProfession.textContent;
 };
 
-function openAddModal() {
-  openPopup(popupAddCard);
+function renderCards({ name, link }) {
+  const cardsElement = cardsTemplate.querySelector(".element").cloneNode(true);
+  const likeBtn = cardsElement.querySelector('.element__button_like');
+  const deleteBtn = cardsElement.querySelector('.element__button_delete');
+  cardsElement.querySelector(".element__info").textContent = name;
+  cardsElement.querySelector(".element__photo").src = link;
+  elements.prepend(cardsElement);
+  likeBtn.addEventListener('click', ActiveLikeBtn);
+  deleteBtn.addEventListener('click', DeleteCard);
+  cardsElement.querySelector(".element__photo").addEventListener("click", function () {
+  popupZoomPhoto.src = link;
+  popupZoomTitle.textContent = name;
+  popupZoomCard.classList.add("popup_opened");
+  });
+
 };
 
-formElementEdit.addEventListener('submit', formSubmitHandlerEdit); 
-editButton.addEventListener('click', openEditModal);
-AddButton.addEventListener('click', openAddModal);
-closeBtnEdit.addEventListener('click', () => closePopup(popupEdit));
-closeBtnAdd.addEventListener('click', () => closePopup(popupAddCard));
+function openZoomModal() {
+  openPopup(popupZoomCard);
+};
 
-//cards render 
-
-const cardsTemplate = document.querySelector("#cardTemplate").content;
+function closeZoomModal() {
+  popupZoomCard.classList.remove("popup_opened");
+}
 
 const cardsInfo = initialCards.map(function (item) {
   return {
@@ -82,51 +107,27 @@ const cardsInfo = initialCards.map(function (item) {
   };
 });
 
-function render() {
-  cardsInfo.forEach(renderCards);
-};
-
 const ActiveLikeBtn = (event) => {
-  event.target.closest('.element__button_like').classList.toggle('element__button_like_active')
+  event.target.closest('.element__button_like').classList.toggle('element__button_like_active');
 };
 
 const DeleteCard = (event) => {
   event.target.closest('.element').remove();
 };
 
-function renderCards({ name, link }) {
-  const cardsElement = cardsTemplate.querySelector(".element").cloneNode(true);
-  cardsElement.querySelector(".element__info").textContent = name;
-  cardsElement.querySelector(".element__photo").src = link;
-  elements.prepend(cardsElement);
-
-  const likeBtn = cardsElement.querySelector('.element__button_like')
-  likeBtn.addEventListener('click', ActiveLikeBtn)
-
-  const deleteBtn = cardsElement.querySelector('.element__button_delete')
-  deleteBtn.addEventListener('click', DeleteCard)
-}
-render();
-
-// add cards 
-
-const addCard = document.querySelector('.popup__save')
-
 const formSubmitHandlerAdd = (evt) => {
   evt.preventDefault();
-  renderCards({ name: inputPlace.value, link: inputImage.value })
+  renderCards({ name: inputPlace.value, link: inputImage.value });
   closePopup(popupAddCard);
   evt.target.reset();
 };
 
+
 popupAddCard.addEventListener('submit', formSubmitHandlerAdd);
-
-
-
-//like card 
-
-//const likebtn = cardsTemplate.querySelector(".element__button_like");
-
-//likebtn.addEventListener('click',function (evt) {
-  //evt.target.classList.toggle('element__button_like_active');
-//});
+formElementEdit.addEventListener('submit', formSubmitHandlerEdit); 
+editButton.addEventListener('click', openEditModal);
+AddButton.addEventListener('click', openAddModal);
+closeBtnEdit.addEventListener('click', () => closePopup(popupEdit));
+closeBtnAdd.addEventListener('click', () => closePopup(popupAddCard));
+ClosePopupImg.addEventListener('click', closeZoomModal)
+render();

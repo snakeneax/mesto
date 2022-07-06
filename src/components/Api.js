4c6b30dd568e0e70f797b345f88ebfc469468345
snakeneax <kuzmin.snakeneax@yandex.ru>
2022-07-06 18:32:00 +0500

    добавил gh-pages

---
+++ src/components/Api.js
@@ -0,0 +1,22 @@
+export default class Api {
+  constructor(options) {
+
+  }
+
+  getInitialCard () {
+
+  }
+
+  
+}
+
+
+
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-44',
+  headers: {
+    authorization: 'd61fdded-02a0-493a-acc6-abf5a9809994',
+    'Content-Type': 'application/json'
+  }
+}); 
